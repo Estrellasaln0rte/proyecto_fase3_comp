115832ec83f763f17984d28c78f918cc34f61b33
Nicole Lee <158302446+Estrellasaln0rte@users.noreply.github.com>
2026-05-16 23:31:33 -0600

    Adición de VOID y verificación entre la declaración de const y  funcion
    corrección de condición para la verificación entre una declaración de una función y una constante.
    
    Se agrego VOID como tipo de dato.

--- MiniLangParser.py
+++ MiniLangParser.py
@@ -56,17 +56,18 @@ def p_declaracion_variable_funcion(p):
     #producciones para const
     elif len(p) == 5 and p[1] == 'const': 
         p[0] = ('decl_const_simple', p[2], p[3], p.lineno(3), p.lexpos(3))
-    elif len(p) == 7: 
+    elif len(p) == 7 and p[1] == 'const': 
         p[0] = ('decl_const_comp', p[2], p[3], p[5], p.lineno(3), p.lexpos(3))
-    #
-    elif len(p) == 7 and p[1] != 'const': 
+    elif len(p) == 7 and p[1]!= 'const': 
         p[0] = ('decl_var_func', p[1], p[2], p[4], p[6], p.lineno(2), p.lexpos(2))
+    #
 
 def p_tipo(p): 
     '''tipo : INT
             | FLOAT
             | STRING 
-            | BOOL'''
+            | BOOL
+            | VOID '''
     p[0] = p[1]
 
 def p_parametros(p): 
